ad82525fa74efdf558cab780b2734ae7eac68165
rastogiriya <rastogiriya081@gmail.com>
2023-09-04 17:29:52 +0530

    basic setup completed

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,31 @@
+import express from "express";
+const router=express.Router();
+import { getProducts,getProductById } from "../controllers/productControllers.js";
+//import asyncHandler from "../middleware/asyncHandler.js";
+//import Product from "../models/productModel.js";
+//import products from '../data/products.js';
+
+// router.get(
+//     '/', asyncHandler (async (req,res)=>{
+//     const products=await Product.find({});
+//     throw new Error('Some error');
+//     res.json(products);
+// })
+// );
+
+// router.get(
+//     '/:id',
+//      asyncHandler(async(req,res) =>{
+//     const product = await Product.findById(req.params.id);
+
+//     if(product){
+//      return res.json(product);
+//     }
+//     res.status(404);
+//    throw new Error ('Resource not found');
+// })
+// );
+
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
+export default router;
