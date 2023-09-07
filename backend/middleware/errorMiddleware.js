3f4dd122b48b20092dd03b97a12e3e3636bb2dfd
Anubhav Pandey <akp99376@gmail.com>
2023-09-07 22:11:49 +0530

    redux setup completed

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -4,20 +4,20 @@ const notFound = (req, res, next) => {
   next(error);
 };
 
-const errorHandler=(err,req,res,next) =>{
-    let statusCode=res.statusCode === 200 ? 500 :res.statusCode;
-    let message=err.message; 
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
 
-    //Check for Mongoose bad Object Id
-    if(err.name === 'CastError ' && err.Kind ==='ObjectId'){
-        message=`Resource not found`;
-        statusCode=404;
-    }
+  //Check for Mongoose bad Object Id
+  if (err.name === "CastError " && err.Kind === "ObjectId") {
+    message = `Resource not found`;
+    statusCode = 404;
+  }
 
-    res.status(statusCode).json({
-        message,
-        stack:process.env.NODE_ENV === 'production' ? ' ' : err.stack,
-    });
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? " " : err.stack,
+  });
 };
 
-export {notFound,errorHandler};
+export { notFound, errorHandler };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,26 +1,29 @@
 import mongoose from "mongoose";
-const userSchema =new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
     },
-isAdmin:{
-    type:Boolean,
-    required:true,
-    default:false,
-},
-},{
-    timestamps:true,
-});
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User=mongoose.model("Uder",userSchema);
+const User = mongoose.model("Uder", userSchema);
 export default User;
