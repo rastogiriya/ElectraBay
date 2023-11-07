1b49861a71458b7d95effbe444415b9ed05b68f6
Anubhav Pandey <akp99376@gmail.com>
2023-11-07 17:24:58 +0530

    payement

--- frontends/src/screens/LoginScreen.jsx
+++ frontends/src/screens/LoginScreen.jsx
@@ -1,63 +1,63 @@
-import {useState,useEffect} from 'react';
-import{Link,useLocation,useNavigate} from 'react-router-dom';
-import {Form,Button,Row,Col} from 'react-bootstrap';
-import { useDispatch,useSelector } from 'react-redux';
-import FormContainer from '../components/FormContainer';
-import Loader from '../components/Loader';
-import { useLoginMutation } from '../slices/usersApiSlice';
-import { setCredentials } from '../slices/authSlice';
-import { Toast } from 'react-toastify';
-
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import { Toast } from "react-toastify";
 
 const LoginScreen = () => {
-    const [email,setEmail]=useState('');
-    const [password,setPassword]=useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const dispatch=useDispatch();
-    const navigate=useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [login,{isLoading}]=useLoginMutation();
+  const [login, { isLoading }] = useLoginMutation();
 
-    const {userInfo }=useSelector((State) =>state.auth);
+  const { userInfo } = useSelector((state) => state.auth);
 
-    const submitHandler=(e) =>{
-        e.preventDefault()
-        console.log('submit')
-    }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
 
   return (
     <FormContainer>
-    <h1>Sign In</h1>
-    <Form onSubmit={submitHandler}>
-        <Form.Group controlId='email' className="my-3">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control 
-            type='email'
-            placeholder='Enter email'
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email" className="my-3">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
             value={email}
-            onChange={(e)=> setEmail(e.target.value)}>
-            </Form.Control>
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
         </Form.Group>
 
-
-        <Form.Group controlId='password' className="my-3">
-            <Form.Label>password</Form.Label>
-            <Form.Control 
-            type='password'
-            placeholder='Enter password'
-            value={password} 
-            onChange={(e)=> setPassword(e.target.value)}>
-            </Form.Control>
+        <Form.Group controlId="password" className="my-3">
+          <Form.Label>password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
         </Form.Group>
 
-        <Button type='submit' variant='primary' className="mt-2">Sign In
-        </Button>       
-    </Form>
+        <Button type="submit" variant="primary" className="mt-2">
+          Sign In
+        </Button>
+      </Form>
 
-    <Row className='py-3'>
-        <Col>New Customer?<Link to='/register'>Register</Link>
+      <Row className="py-3">
+        <Col>
+          New Customer?<Link to="/register">Register</Link>
         </Col>
-    </Row>
+      </Row>
     </FormContainer>
   );
 };
