72bdf274e0ba21b48bd9cf52058400754769ba25
rastogiriya <rastogiriya081@gmail.com>
2023-11-11 23:53:23 +0530

    orderscreens

--- frontends/src/screens/OrderScreen.jsx
+++ frontends/src/screens/OrderScreen.jsx
@@ -41,6 +41,34 @@ const OrderScreen=()=>{
                       <Message variant='danger'>Not Delivered</Message>  
                     )}
                 </ListGroup.Item>
+
+                <ListGroup.Item>
+                    <h2>Payment Method</h2>
+                    <p>
+                        <strong>Method:</strong>
+                        {order.paymentMethod}
+                    </p>
+                    {order.isPaid?(
+                        <Message variant='success'>
+                            Paid on{order.paidAt}
+                        </Message>
+                    ):(
+                      <Message variant='danger'>Not Paid</Message>  
+                    )}
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                <h2>Order Items</h2>
+        {order.orderItems.map((item,index)=>(
+                    <ListGroup.Item Key={index}>
+                        <Row>
+                            <Col md={1}>
+                                <Image src={item.image} alt={item.name} fluid rounded/>
+                            </Col>
+                        </Row>
+                     </ListGroup.Item>
+                    ))}
+                </ListGroup.Item>
             </ListGroup>
         </Col>
         <Col md={4}>Column</Col>
